af61a4a0b4d89f29c4bd989c9019438510a46322
BensonB12 <89985506+BensonB12@users.noreply.github.com>
2024-01-23 15:14:40 -0700

    New Controller

---
+++ BlazorApp1/Controllers/BookController.cs
@@ -0,0 +1,29 @@
+﻿using BlazorApp1.Data;
+using Microsoft.AspNetCore.Mvc;
+using RazorClassLibrary1.Data;
+
+namespace BlazorApp1.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BookController : Controller
+{
+    FakeDbContext _context;
+    public BookController(FakeDbContext c)
+    {
+        _context = c;
+    }
+
+    [HttpGet("all")]
+    public async Task<IEnumerable<Book>> GetAllBooks()
+    {
+        return await _context.GetAllBooks();
+    }
+
+    [HttpGet("new/{bookTitle}")]
+    public async Task<IEnumerable<Book>> GetAllBooks(string bookTitle)
+    {
+        await _context.AddABook(new(bookTitle));
+        return await _context.GetAllBooks();
+    }
+}
